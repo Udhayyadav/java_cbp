70e07e5507b7d1e02ac6dda0d24bc07a1eb9c910
Udhayyadav <133828293+Udhayyadav@users.noreply.github.com>
2023-12-22 22:19:59 +0530

    Add files via upload

--- playlist_main.java
+++ playlist_main.java
@@ -34,6 +34,9 @@ public class playlist_main {
 
     public static void main(String[] args) {
 
+        Scanner sc=new Scanner(System.in);
+
+        int choice;
         LinkedList<String> Song = new LinkedList<String>();
    
         Song s1 = new Song("mimmi",4.15);
@@ -45,16 +48,48 @@ public class playlist_main {
 
         songs.add(s2);
 
+        Song_iterator play=new Song_iterator();
+
+
+
     //    System.out.println(songs.get(0));
         // System.out.println("first songin the playlist:");
         // System.out.println("name:" + songs.get(0).name);
         // System.out.println("duration:" + songs.get(0).duration);
         // songs.get(0).print();
 
-        for(Song i:songs)
-        {
-            i.print();
-        }
+
+        do{
+
+            System.out.println("enter any option ");
+            System.out.println("1)play next song ");
+            System.out.println("2)enter previous song ");
+            System.out.println("3)Show all Songs ");
+
+            choice = sc.nextInt();
+
+            switch (choice)
+            {
+                case 1:{
+                    
+                     play.nextsong();
+                     break;
+                }
+                case 2:
+                {
+                    play.previoussong();
+                    break;
+                }
+                case 3:
+                {
+                    for(Song it:songs)
+                     {
+                       it.print();
+                    }
+                }
+            }
+
+        }while(choice!=-1);
 
     }
 }
